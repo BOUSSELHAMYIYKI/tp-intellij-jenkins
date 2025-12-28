8fd82de90c4068fdb6b2f84e6ceb3fe050a71a65
Bousselham el-abd <bousselham.elabd2000@gmail.com>
2025-12-28 23:13:22 +0100

    first commit

--- src/main/java/org/example/tp1.java
+++ src/main/java/org/example/tp1.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class tp1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("دخل الاسم ديالك: ");
+        String nom = sc.nextLine();
+
+        System.out.print("دخل العمر ديالك: ");
+        int age = sc.nextInt();
+
+        int ageFuture = age + 10;
+
+        System.out.println("مرحبا " + nom);
+        System.out.println("العمر ديالك دابا: " + age);
+        System.out.println("العمر ديالك من بعد 10 سنين: " + ageFuture);
+
+        sc.close();
+    }
 }
