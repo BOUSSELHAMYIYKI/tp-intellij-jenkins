1dc321461de84d728e9145647684456e4aa87273
Bousselham el-abd <bousselham.elabd2000@gmail.com>
2025-12-29 16:26:00 +0100

    nine  commit

--- src/test/org/example/tp1Test.java
+++ src/test/org/example/tp1Test.java
@@ -1,20 +1,15 @@
 package org.example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class tp1Test {
-
-    @BeforeEach
-    void setUp() {
-    }
 
     @Test
     void testCalculerAgeFuture() {
         assertEquals(20, tp1.calculerAgeFuture(10));
         assertEquals(35, tp1.calculerAgeFuture(25));
         assertEquals(10, tp1.calculerAgeFuture(0));
+        assertEquals(5, tp1.calculerAgeFuture(-5));
     }
 }
